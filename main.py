22e9fa1db5e0a3cfc3a4cd366d3267de1ea7ac3b
DaDimpfl <damien.dimpfl@web.de>
2025-07-30 19:11:59 +0200

    Working pw generator

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import random
+from random import randint, choice
+from tkinter import Tk
+from tkinter import messagebox
 from constants import *
 
 def main():
-    word_list = get_strings(WORD_LIST)
-    sp_char = get_strings(CHAR_LIST)
 
-    password = assemble_pw(word_list, sp_char, PASSWORD_LENGTH)
+    password = assemble_pw(get_strings(WORD_LIST), get_strings(CHAR_LIST), PASSWORD_LENGTH)
+    
+    addToClipBoard(password)
 
-    print(password)
 
 #Returns a list of strings
 def get_strings(filepath):
@@ -19,15 +20,25 @@ def assemble_pw(word_list, char_list, pw_length):
     password = ""
     counter = 1
     #Randomize which word will be capitalized
-    capitalize = random.randint(counter, pw_length)
+    capitalize = randint(counter, pw_length)
     while counter <= pw_length:
         if counter == capitalize:
-            password += random.choice(word_list).capitalize() + random.choice(char_list)
+            password += choice(word_list).capitalize() + choice(char_list)
         else:
-            password += random.choice(word_list) + random.choice(char_list)
+            password += choice(word_list) + choice(char_list)
         counter += 1
     #Cut the last special char for aesthetics
     return password[:-1]
 
+def addToClipBoard(string):
+    #Get the string into the clipboard and show message with string
+    r = Tk()
+    r.withdraw()
+    r.clipboard_clear()
+    r.clipboard_append(string)
+    messagebox.showinfo("Password", f"The new password \n\n\"{string}\" \n\nhas been added to youre clipboard")
+    r.update() #To keep password in clipboard after the window closes
+    r.destroy()
+    
 if __name__ == "__main__":
     main()
