cfa4085105ea92c2ecbe8698ad8f7f5415c69fd2
DaDimpfl <damien.dimpfl@web.de>
2025-07-30 18:04:11 +0200

    Prints password to console and deleted hyphons in wordlist

--- constants.py
+++ constants.py
@@ -1,1 +1,3 @@
 PASSWORD_LENGTH = 5
+WORD_LIST = "wellknownwords_without_hyphon.txt"
+CHAR_LIST = "spchar.txt"
--- main.py
+++ main.py
@@ -1,7 +1,33 @@
+import random
 from constants import *
 
 def main():
-    pass
+    word_list = get_strings(WORD_LIST)
+    sp_char = get_strings(CHAR_LIST)
+
+    password = assemble_pw(word_list, sp_char, PASSWORD_LENGTH)
+
+    print(password)
+
+#Returns a list of strings
+def get_strings(filepath):
+    with open(filepath) as f:
+        return f.read().split("\n")
+
+#Returns the assembled password with special chars and numbers
+def assemble_pw(word_list, char_list, pw_length):
+    password = ""
+    counter = 1
+    #Randomize which word will be capitalized
+    capitalize = random.randint(counter, pw_length)
+    while counter <= pw_length:
+        if counter == capitalize:
+            password += random.choice(word_list).capitalize() + random.choice(char_list)
+        else:
+            password += random.choice(word_list) + random.choice(char_list)
+        counter += 1
+    #Cut the last special char for aesthetics
+    return password[:-1]
 
 if __name__ == "__main__":
     main()
